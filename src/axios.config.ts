d46f53a425f40f0d3c60da806fcb7f414e362d8e
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-09-24 23:16:38 +0100

    FIXES

--- src/axios.config.ts
+++ src/axios.config.ts
@@ -1,6 +1,4 @@
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config()
-axios.defaults.baseURL = process.env.ENVIRONMENT = "production" ? "/api/v1" : "http://127.0.0.1:8405/api/v1";
+axios.defaults.baseURL =  "http://127.0.0.1:8405/api/v1";
 axios.defaults.timeout = 3500;
 // axios.defaults.withCredentials = true;
