6e40f72e9e38c4dac4d27935cb8fd866f53ab9c6
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-10-18 18:25:37 +0100

    Experimenting With Capacitor.js

---
+++ android/app/src/main/java/com/drizzles/360Devs/MainActivity.java
@@ -0,0 +1,5 @@
+package com.drizzles.360Devs;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.drizzles.360Devs',
+  appName: '360 Devs',
+  webDir: './dist',
+  bundledWebRuntime: false
+};
+
+export default config;
